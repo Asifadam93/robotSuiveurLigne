783a994da01bb8414780eb906262916148140c14
PetitGrigri <fgriselles@hotmail.fr>
2017-02-01 22:06:09 +0100

    programme principal terminé, début priorité

--- Moteur/Moteur.cpp
+++ Moteur/Moteur.cpp
@@ -17,38 +17,53 @@ void Moteur::stop(){
 }
 
 void Moteur::tournerGauche(int force){
+    if (force == 0) {
+        M2 = false;
+        E2 = vitesseReference/2;
+        M1 = true;
+        E1 = vitesseReference/2;
+        return;
+    }
     M1 = true;
     E1 = vitesseReference;
     
     if ((force >= 1) && (force <=4)) {
         M2 = true;
-        E2 = vitesseReference - ((vitesseReference/4) * force);
+        E2 = vitesseReference - ((vitesseReference/float(4)) * float(force));
     }
     if (force >=4 ) {
         E1 = vitesseMax;
     }
-    if (force >= 5) {
+    if (force == 5) {
         M2 = false;
-        E2 = vitesseReference /2;
+        E2 = vitesseReference /float(2);
     }
+
 }
 
 void Moteur::tournerDroite(int force){
+    if (force == 0) {
+        M2 = true;
+        E2 = vitesseReference/2;
+        M1 = false;
+        E1 = vitesseReference/2;
+        return;
+    }
     M2 = true;
     E2 = vitesseReference;
     
     if ((force >= 1) && (force <=4)) {
         M1 = true;
-        E1 = vitesseReference - ((vitesseReference/4) * force);
+        E1 = vitesseReference - ((vitesseReference/float(4)) * float(force));
     }
     if (force >=4 ) {
         E2 = vitesseMax;
     }
     if (force >= 5) {
         M1 = false;
-        
-        E1 = vitesseReference /2;
+        E1 = vitesseReference /float(2);
     }
+
 }
 
 
--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "PhotoResistance.hpp"
 #include "Led.hpp"
 #include "Moteur.hpp"
+#include "InfraRouge.hpp"
+
 
 //boolean informations spécifique sur le parcours
 bool suspicionPrioriteADroite   = false;
@@ -22,12 +24,13 @@ double tempsAttenteSortie;
 double tempsAttentePriorite;
 
 //permet de mémoriser la dernière direction prise par le robot
-int lastDir = ' ';
+int lastDir = 0;
 
 //instanciation des objets
 PhotoResistance myPR;
 Led myLed;
 Moteur myMoteur;
+InfraRouge myIR;
 Serial serialOut(SERIAL_TX, SERIAL_RX);
 DigitalIn blueButton(USER_BUTTON);
 
@@ -60,17 +63,22 @@ int main() {
         refreshCapteur();
         
         //TEST PRIORITE
-        tempsAttentePriorite = (clock() - timerPriorite);
 
-        if (tempsAttentePriorite > 10) {
-            prioriteADroite = false;
-            suspicionPrioriteADroite = false;
-        }
         /**
           * Gestion de la détection des racourcis ou des priorités
           */
+          //TODO AMELIORER DETECTION ET RALENTIR (si possible)
+        tempsAttentePriorite = (clock() - timerPriorite);
+
+        if (tempsAttentePriorite > 10) {
+            suspicionPrioriteADroite = false;
+        }
+        if (tempsAttentePriorite > 20) {
+            prioriteADroite = false;
+        }
+                    
         //gestion de la priorité  
-        if ((cdOld  && !cd ) &&
+        if ((cdOld  && !cd  ) &&
             (suspicionPrioriteADroite == true) && (tempsAttentePriorite < 10) ) {
             myLed.turnOffAllLed();
             wait(0.05);
@@ -79,7 +87,7 @@ int main() {
             prioriteADroite = true;
             suspicionPrioriteADroite = false;
         }
-        if (!cdOld  && cd ) {
+        if ((!cdOld  && cd && !prioriteADroite) && !(cd && d && c && g && cg) && (lastDir >3))  {
 
             suspicionPrioriteADroite = true;
             prioriteADroite          = false;
@@ -92,72 +100,98 @@ int main() {
         /**
          *  Déplacement
          */  
-        if  ((!g && c && !d) || (g && c && d)) {
+        if (prioriteADroite && (myIR.isDroiteDetected() || myIR.isCentreDetected())) {
+             while(myIR.isDroiteDetected() || myIR.isCentreDetected()|| myIR.isGaucheDetected() ) {
+                myMoteur.stop();
+                wait(1);
+             }
+        } else if  ((!g && c && !d) || (g && c && d)) {
             myMoteur.avancer();
             lastDir = 5;
         } else if( cd && !d) {
             myMoteur.tournerGauche(4);
             lastDir = 1;
-        } else if( cd && d) {
+        } else if( cd && d ) {
             myMoteur.tournerGauche(3);
             lastDir = 2;
         } else if( d && !c) {
             myMoteur.tournerGauche(2);
             lastDir = 3;
-        } else if( d && c) {
+        } else if( d && c ) {
             myMoteur.tournerGauche(1);
             lastDir = 4;
         } else if( cg && !g) {
             myMoteur.tournerDroite(4);
             lastDir = 9;
-        } else if( cg && g) {
+        } else if( cg && g ) {
             myMoteur.tournerDroite(3);
             lastDir = 8;
         } else if( g && !c) {
             myMoteur.tournerDroite(2);
             lastDir = 7;
-        } else if( g && c) {
+        } else if( g && c ) {
             myMoteur.tournerDroite(1);
             lastDir = 6;
         //fonctionnement sortie ou fin du parcours
         } else {
-            if ( (lastDir == 1) || (lastDir == 2) ) {
+            if (( lastDir != 0 ) && (( lastDir > 3 ) && ( lastDir < 7 ))){
+
+                //DEBUT CAUSE PROBLEME
                 timerSortie = clock();
-                tempsAttenteSortie = clock() - timerSortie;
+                tempsAttenteSortie = (double)(clock() - timerSortie);
+
+                while((tempsAttenteSortie < 100) && !d) {
+                    myMoteur.tournerGauche(0); // mef, tournerGauche(5) ne fait pas rester sur place : 1, -0.4, à voir si ca ne cause pas problème
+                    tempsAttenteSortie = (double)(clock() - timerSortie);
+                    refreshCapteur();
+                }  
+ 
+                while((tempsAttenteSortie < 270) && !g) {
+                    myMoteur.tournerDroite(0);  // mef, tournerDroite(5) ne fait pas rester sur place : 1, -0.4, à voir si ca ne cause pas problème
+                    tempsAttenteSortie = (double)(clock() - timerSortie);
+                    refreshCapteur();
+                }
+
+                if ((tempsAttenteSortie >= 270) || d || g) {
+                    lastDir = 0;
+                    myMoteur.stop();
+                    wait(2);
+                }
+                    
+                //FIN CAUSE PROBLEME
+            } else if (( lastDir <= 3 ) && (lastDir >0)) {
+                timerSortie = clock();
+                tempsAttenteSortie = (double)(clock() - timerSortie);
                 
                 while((tempsAttenteSortie < 60) && (!d)) {
-                    if (tempsAttenteSortie <= 20) {
+                    if (tempsAttenteSortie <= 40) {
                         myMoteur.tournerGauche(4);
                     } else {
                         myMoteur.tournerGauche(5);
                     }
-                    tempsAttenteSortie = clock() - timerSortie;
+                    tempsAttenteSortie = (double)(clock() - timerSortie);
                     refreshCapteur(); 
                 }
-                lastDir = ' ';
-
-            } else if ( (lastDir == 8) || (lastDir == 9) ) {
+                if (tempsAttenteSortie >= 60)
+                    lastDir = 0;
+            } else if ( lastDir >= 7 ) {
                 timerSortie = clock();
-                tempsAttenteSortie = clock() - timerSortie;
+                tempsAttenteSortie = (double)(clock() - timerSortie);
                 
                 while((tempsAttenteSortie < 60) && (!g)) {
-                    if (tempsAttenteSortie <= 20) {
+                    if (tempsAttenteSortie <= 40) {
                         myMoteur.tournerDroite(4);
                     } else {
                         myMoteur.tournerDroite(5);
                     }
-                    tempsAttenteSortie = clock() - timerSortie;
+                    tempsAttenteSortie = (double)(clock() - timerSortie);
                     refreshCapteur();
                 }
-                
-                lastDir = ' ';
-
+                if (tempsAttenteSortie >= 60)
+                    lastDir = 0;
             } else {
                 myMoteur.stop();
-                /*while(!(blueButton == 0)) {
-                    serialOut.printf("%d - %d - %d - %d - %d * lastDir : %c tempsAttenteSortie : %f\n\r", cg, g, c, d, cd, lastDir, tempsAttenteSortie);
-                    wait(1);
-                }*/
+                /**/
             } 
         }
 
